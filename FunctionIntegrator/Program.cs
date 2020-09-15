de9d22bd3c2dc9c14523815c4f46e5f2ee394a27
Infarh <shmachilin@yandex.ru>
2020-09-15 10:48:54 +0300

    Выделение функций

--- FunctionIntegrator/Program.cs
+++ FunctionIntegrator/Program.cs
@@ -11,6 +11,34 @@ namespace FunctionIntegrator
             return result;
         }
 
+        // ref - передача переменной по ссылке с возможностью прочитать её значение внутри метода и изменить его
+        // out - позволяет установить значение переменной внутри метода, но не читать её исходное значение
+        static void ReadValueFromConsole(out double value, string Message)
+        {
+            string str;
+            do
+            {
+                Console.Write(Message);
+                str = Console.ReadLine();
+            }
+            while (!double.TryParse(str, out value));
+        }
+
+        static double GetIntegralValue(double a, double b, double dx)
+        {
+            double sum = 0;
+            var x = a;
+
+            while (x <= b)
+            {
+                sum += Function(x) * dx;
+
+                x += dx; // x = x + dx;
+            }
+
+            return sum;
+        }
+
         static void Main(string[] args)
         {
             // f(x) = 3 * x^2; [0; 1]
@@ -19,29 +47,9 @@ namespace FunctionIntegrator
             double b = 1;
             double dx = 0.0001;
 
-            string a_str;
-            do
-            {
-                Console.Write("Введите значение a > ");
-                a_str = Console.ReadLine();
-            }
-            while (!double.TryParse(a_str, out a));
-
-            string b_str;
-            do
-            {
-                Console.Write("Введите значение b > ");
-                b_str = Console.ReadLine();
-            }
-            while (!double.TryParse(b_str, out b));
-
-            string dx_str;
-            do
-            {
-                Console.Write("Введите значение dx > ");
-                dx_str = Console.ReadLine();
-            }
-            while (!double.TryParse(dx_str, out dx));
+            ReadValueFromConsole(out a, "Введите значение a > ");
+            ReadValueFromConsole(out b, "Введите значение b > ");
+            ReadValueFromConsole(out dx, "Введите значение dx > ");
 
             if (a == b)
             {
@@ -55,24 +63,26 @@ namespace FunctionIntegrator
                 return;
             }
 
-            double sum = 0;
-            var x = a;
+            //double sum = 0;
+            //var x = a;
 
-            int step_count = 0;
-            while (x <= b)
-            {
-                step_count++;
-                var f = Function(x);
+            //int step_count = 0;
+            //while (x <= b)
+            //{
+            //    step_count++;
+            //    var f = Function(x);
 
-                var dv = f * dx;
+            //    var dv = f * dx;
 
-                sum += dv;
+            //    sum += dv;
 
-                x += dx; // x = x + dx;
-            }
+            //    x += dx; // x = x + dx;
+            //}
+
+            var sum = GetIntegralValue(a, b, dx);
 
             Console.WriteLine("Интеграл функции f(x) = {0}", sum);
-            Console.WriteLine("Число шагов интегрирования {0}", step_count);
+            //Console.WriteLine("Число шагов интегрирования {0}", step_count);
         }
     }
 }
