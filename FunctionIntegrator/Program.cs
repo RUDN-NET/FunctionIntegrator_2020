58690730c55821c8bfe5c17a6b73464c3a6d4193
Infarh <shmachilin@yandex.ru>
2020-09-15 09:50:55 +0300

    Тестирование возможности передачи аргументов командной строки в прлиожение

--- FunctionIntegrator/Program.cs
+++ FunctionIntegrator/Program.cs
@@ -4,9 +4,21 @@ namespace FunctionIntegrator
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            
+            // f(x) = 3 * x^2; [0; 1]
+
+            //const double a = 0;
+            //const double b = 1;
+
+            Console.WriteLine("В программу передано аргументов " + args.Length);
+
+            for (var i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Аргумент " + i + " = " + args[i]);
+            }
         }
     }
 }
