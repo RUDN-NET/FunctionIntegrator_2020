cdcaf6d0c78c0cad222f5538f1895780108090fc
Infarh <shmachilin@yandex.ru>
2020-09-15 09:51:52 +0300

    Удаление тестового кода

--- FunctionIntegrator/Program.cs
+++ FunctionIntegrator/Program.cs
@@ -10,15 +10,8 @@ namespace FunctionIntegrator
         {
             // f(x) = 3 * x^2; [0; 1]
 
-            //const double a = 0;
-            //const double b = 1;
-
-            Console.WriteLine("В программу передано аргументов " + args.Length);
-
-            for (var i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine("Аргумент " + i + " = " + args[i]);
-            }
+            const double a = 0;
+            const double b = 1;
         }
     }
 }
